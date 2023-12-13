a1acd5a5effb88f0477aa7ff3c7be744b335c810
joshuaedo <joshua.edo01@gmail.com>
2023-12-13 19:21:50 +0100

    feat: integrate toast & dynamic comments

--- components/ui/comments/CommentSection.tsx
+++ components/ui/comments/CommentSection.tsx
@@ -4,80 +4,59 @@ import { usePathname } from "next/navigation"
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 import { MessageCircle, Share } from "lucide-react"
-import { useSession } from "next-auth/react"
-
 import { siteConfig } from "@/config/site"
+import { formatTimeToNow, getRandomNumber } from "@/lib/utils"
+import { toast } from "@/hooks/use-toast"
 import UserAvatar from "../UserAvatar"
 import CreateComment from "./CreateComment"
-
-interface CommentSectionProps {}
 
 interface ToggleCommentsAndShareProps {
   isModalOpen: boolean
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const { title, email, images } = siteConfig
-
-const comments = Array.from({ length: 10 }, (_, index) => ({
-  text: `Comment ${
-    index + 1
-  }: My employer and other large employers in my region still conduct some traditional performance reviews. They do feature negative comments.`,
-  author: {
-    name: title,
-    email: email,
-    image: images[0],
-  },
-  date: "2 days ago",
-  replies: [
-    {
-      text: "This is a reply",
-      author: {
-        name: "Reply Author",
-        image: "https://i.pravatar.cc/300",
-      },
-      date: "2 days ago",
-    },
-  ],
-}))
+const { url } = siteConfig
 
 const CommentSection = ({}) => {
   const pathname = usePathname()
   const slug = pathname.replace("/post/", "")
-  // const { data: session } = useSession();
+
   const { data, isPending } = useQuery({
     queryFn: async () => {
       const { data } = await axios.get(`/api/comments/get?slug=${slug}`)
-      return data
+      return data as PostComments
     },
     queryKey: ["comments", slug],
   })
 
-  console.log(data)
+  let commentCount = 0
+
+  if (!isPending && data?.comments?.length) {
+    commentCount = data?.comments?.length
+  }
 
   return (
     <div
       id="comment-section"
       className="max-h-[70svh] space-y-4 overflow-auto py-1 pl-3 pr-6 text-sm"
     >
-      <h1 className="text-xl font-medium">
-        Responses ({comments?.length ?? 0})
-      </h1>
+      <h1 className="text-xl font-medium">Responses ({commentCount})</h1>
       <CreateComment slug={slug} />
       <div className="pt-4">
-        {comments.map((comment, index) => (
+        {data?.comments?.map((comment, index) => (
           <div className="space-y-3 border-t py-8" key={index}>
             <div className="flex items-center gap-3">
               <UserAvatar user={comment.author} />
               <div>
                 <h4 className="font-medium">{comment.author.name}</h4>
-                <h6 className="text-muted-foreground">{comment.date}</h6>
+                <h6 className="text-muted-foreground">
+                  {formatTimeToNow(new Date(comment.createdAt))}
+                </h6>
               </div>
             </div>
             <p className="">{comment.text}</p>
           </div>
         ))}
-        c
       </div>
     </div>
   )
@@ -87,6 +66,33 @@ const ToggleCommentsAndShare = ({
   isModalOpen,
   setIsModalOpen,
 }: ToggleCommentsAndShareProps) => {
+  const pathname = usePathname()
+  const slug = pathname.replace("/post/", "")
+  const sharePage = `${url}${pathname}`
+
+  const { data, isPending } = useQuery({
+    queryFn: async () => {
+      const { data } = await axios.get(`/api/comments/get?slug=${slug}`)
+      return data
+    },
+    queryKey: ["comments-count", slug],
+  })
+
+  let commentCount = 0
+
+  if (!isPending && data?.comments?.length) {
+    commentCount = data?.comments?.length
+  }
+
+  const randomShareCount = getRandomNumber()
+
+  const handleSharePage = () => {
+    navigator.clipboard.writeText(sharePage)
+    toast({
+      description: "Url successfully copied to clipboard.",
+    })
+  }
+
   return (
     <div className="mt-9 flex w-fit items-center gap-6 px-6 text-sm  font-normal md:px-12 lg:mt-6 lg:px-64">
       <div
@@ -94,11 +100,14 @@ const ToggleCommentsAndShare = ({
         className="flex cursor-pointer items-center gap-1 opacity-70 hover:opacity-100"
       >
         <MessageCircle className="h-4 w-4" strokeWidth={0.9} />
-        <span className="mt-[0.9]">( 23 )</span>
+        <span className="mt-[0.9]">( {commentCount} )</span>
       </div>
-      <div className="flex cursor-pointer items-center gap-1 opacity-70 hover:opacity-100">
+      <div
+        onClick={handleSharePage}
+        className="flex cursor-pointer items-center gap-1 opacity-70 hover:opacity-100"
+      >
         <Share className="h-4 w-4" strokeWidth={0.9} />
-        <span className="mt-[0.9]">( 64 )</span>
+        <span className="mt-[0.9]">( {randomShareCount} )</span>
       </div>
     </div>
   )
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,35 +1,91 @@
 import { clsx, type ClassValue } from "clsx"
+import { formatDistanceToNowStrict } from "date-fns"
+import locale from "date-fns/locale/en-US"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+const formatDistanceLocale = {
+  lessThanXSeconds: "just now",
+  xSeconds: "just now",
+  halfAMinute: "just now",
+  lessThanXMinutes: "{{count}}m",
+  xMinutes: "{{count}}m",
+  aboutXHours: "{{count}}h",
+  xHours: "{{count}}h",
+  xDays: "{{count}}d",
+  aboutXWeeks: "{{count}}w",
+  xWeeks: "{{count}}w",
+  aboutXMonths: "{{count}}m",
+  xMonths: "{{count}}m",
+  aboutXYears: "{{count}}y",
+  xYears: "{{count}}y",
+  overXYears: "{{count}}y",
+  almostXYears: "{{count}}y",
+}
+
+function formatDistance(token: string, count: number, options?: any): string {
+  options = options || {}
+
+  const result = formatDistanceLocale[
+    token as keyof typeof formatDistanceLocale
+  ].replace("{{count}}", count.toString())
+
+  if (options.addSuffix) {
+    if (options.comparison > 0) {
+      return "in " + result
+    } else {
+      if (result === "just now") return result
+      return result + " ago"
+    }
+  }
+
+  return result
+}
+
+export function formatTimeToNow(date: Date): string {
+  return formatDistanceToNowStrict(date, {
+    addSuffix: true,
+    locale: {
+      ...locale,
+      formatDistance,
+    },
+  })
+}
+
 interface SplitTitle {
-  title1: string;
-  title2: string;
+  title1: string
+  title2: string
 }
 
 export const splitTitle = (input: string): SplitTitle => {
-  const lastSpaceIndex = input.lastIndexOf(' ');
+  const lastSpaceIndex = input.lastIndexOf(" ")
 
   if (lastSpaceIndex === -1) {
     // If there is no space, return the entire string as title1 and an empty string as title2
-    return { title1: input, title2: '' };
+    return { title1: input, title2: "" }
   }
 
-  const title1 = input.slice(0, lastSpaceIndex);
-  const title2 = input.slice(lastSpaceIndex + 1);
+  const title1 = input.slice(0, lastSpaceIndex)
+  const title2 = input.slice(lastSpaceIndex + 1)
 
-  return { title1, title2 };
-};
+  return { title1, title2 }
+}
 
 export function truncateString(text: string, maxLength: number): string {
   if (text.length <= maxLength) {
-    return text;
+    return text
   } else {
-    return text.slice(0, maxLength) + '...';
+    return text.slice(0, maxLength) + "..."
   }
 }
 
+export function getRandomNumber(): number {
+  const min = 7
+  const max = 28
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
 
+  return randomNumber
+}
